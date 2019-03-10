aab8ee9eafebfa196d73ca5cac89f1453b34f569
JiYuhang <wakuflair@163.com>
2019-03-10 14:07:40 +0800

    1.Sprache

---
+++ README.md
@@ -0,0 +1,9 @@
+## 关于awesome-dotnet-core-learning
+
+.NET Core从2016年发布1.0以来,至今已经3岁了,可以说现在的.NET Core的生态已经相当完善了,可满足各行业的生产要求.如果你还在怀疑.NET Core的实用性质,是时候转变想法了.
+
+在GITHUB上有一个项目:[awesome-dotnet-core](https://github.com/thangchung/awesome-dotnet-core).专门罗列了各种.NET Core优秀的开源库,该列表几乎每天都在增长,.NET Core生态的高速增长由此可见一斑.
+
+为了更好地普及.NET Core,方便.NET开发者,为.NET Core生态建设添砖加瓦,我产生了创建awesome-dotnet-core系列博客的想法.该系列会不定期发布博客,介绍awesome-dotnet-core其中一些实用的,有意思的,~~我能看懂的~~库,尽量以浅显的文字,简单的例子说明库的用法和使用场景等.
+
+1. [Sprache-解析器构建库](blogs/1.Sprache.md)
---
+++ src/1.Sprache/Sample/Sample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Sprache;
+
+namespace Sample
+{
+    class Program
+    {
+        // 标识符解析规则
+        private static Parser<string> Identifier =
+            from leading in Parse.WhiteSpace.Many()                 // 可以包含前置空格
+                from first in Parse.Letter.Once()                   // 第一个字符只能是字母
+                from rest in Parse.LetterOrDigit.Many()             // 剩余的字符可以是字母或数字
+                from trailing in Parse.WhiteSpace.Many()            // 可以包含后置空格
+                select new string(first.Concat(rest).ToArray());    // first+rest做为标识符
+
+        /// <summary>
+        /// 检查输入的文本中是否包含合法的标识符
+        /// </summary>
+        /// <param name="text">文本</param>
+        private static void CheckIdentifier(string text)
+        {
+            var result = Identifier.TryParse(text);
+            if (result.WasSuccessful)
+            {
+                Console.WriteLine($"[{text}]中包含合法的标识符.标识符为: {result.Value}");
+            }
+            else
+            {
+                Console.WriteLine($"[{text}]中不包含合法的标识符.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CheckIdentifier("    a123  ");
+            CheckIdentifier(" 1abc");
+        }
+    }
+}
