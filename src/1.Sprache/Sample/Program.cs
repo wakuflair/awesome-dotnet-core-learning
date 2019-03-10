485efb757ea2856005cf8ad99aa06a5d5f0dd5a8
JiYuhang <wakuflair@163.com>
2019-03-10 14:46:59 +0800

    调整示例工程结构

--- src/1.Sprache/Sample/Program.cs
+++ src/1.Sprache/Sample/Program.cs
@@ -8,11 +8,11 @@ namespace Sample
     {
         // 标识符解析规则
         private static Parser<string> Identifier =
-            from leading in Parse.WhiteSpace.Many()                 // 可以包含前置空格
-                from first in Parse.Letter.Once()                   // 第一个字符只能是字母
-                from rest in Parse.LetterOrDigit.Many()             // 剩余的字符可以是字母或数字
-                from trailing in Parse.WhiteSpace.Many()            // 可以包含后置空格
-                select new string(first.Concat(rest).ToArray());    // first+rest做为标识符
+            from leading in Parse.WhiteSpace.Many()             // 可以包含前置空格
+            from first in Parse.Letter.Once()                   // 第一个字符只能是字母
+            from rest in Parse.LetterOrDigit.Many()             // 剩余的字符可以是字母或数字
+            from trailing in Parse.WhiteSpace.Many()            // 可以包含后置空格
+            select new string(first.Concat(rest).ToArray());    // first+rest做为标识符
 
         /// <summary>
         /// 检查输入的文本中是否包含合法的标识符
